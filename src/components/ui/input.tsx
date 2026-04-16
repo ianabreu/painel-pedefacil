fa9d3b80072624783097d595310dc4adc6d2d3bd
Ian Abreu <ianabreudev@gmail.com>
2026-04-15 21:38:12 -0300

    add basic components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,33 +1,20 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-interface InputProps extends React.ComponentProps<"input"> {
-  label?: string;
-  error?: string;
-}
-function Input({ label, error, className, type, id, ...props }: InputProps) {
+
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
-    <div>
-      {label && (
-        <label htmlFor={id} className="text-sm">
-          {label}
-        </label>
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:bg-input/30",
+        "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
+        "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
+        className,
       )}
-      <input
-        type={type}
-        data-slot="input"
-        id={id}
-        aria-invalid={error ? "true" : "false"}
-        className={cn(
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-          className
-        )}
-        {...props}
-      />
-      {error && <span className="text-xs text-red-500">{error}</span>}
-    </div>
+      {...props}
+    />
   );
 }
 
